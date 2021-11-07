cd32d877b9651c693edcea9b53a0678a65642ae0
Benjamin <benjamin.guy.thomas@gmail.com>
2021-11-07 12:21:29 +0100

    Demo: login without relying on the API

--- cypress/integration/signInForm_spec.js
+++ cypress/integration/signInForm_spec.js
@@ -1,23 +1,70 @@
+function stubSuccess(cy) {
+    cy.intercept({
+            method: 'POST',
+            url: '**/api/users/login',
+        },
+        {
+            statusCode: 200,
+            body: {
+                "user": {
+                    "email": "user@example.com",
+                    "username": "John Doe",
+                    "bio": null,
+                    "image": null,
+                    "token": "123"
+                }
+            }
+        }
+    ).as('loginSuccess');
+}
+
+function stubFailure(cy) {
+    cy.intercept({
+            method: 'POST',
+            url: '**/api/users/login',
+        },
+        {
+            statusCode: 403,
+            body: {"errors": {"Your email or password": ["is invalid"]}}
+        }
+    ).as('loginFailure');
+}
+
 describe("SignIn Form", () => {
-  it("Check SignIn Form fields", () => {
-    cy.visit("/login");
-    cy.get("input[type=email]").should("be.visible");
-    cy.get("input[type=password]").should("be.visible");
-    cy.get("button[type=submit]").should("be.visible");
-    cy.get("div.row a[ui-sref='app.register']")
-      .should("be.visible")
-      .should("have.attr", "href")
-      .and("contain", "register");
-    cy.get("ul.nav a[ui-sref='app.login']")
-      .should("be.visible")
-      .should("have.attr", "href")
-      .and("contain", "login");
-    cy.get("ul.nav a[ui-sref='app.register']")
-      .should("be.visible")
-      .should("have.attr", "href")
-      .and("contain", "register");
-    cy.get("ul.nav a[ui-sref='app.home']")
-      .should("be.visible")
-      .should("have.attr", "href");
-  });
+
+    it("Shows an error on auth failure", () => {
+        cy.visit("/login");
+        cy.location('hash').should('eq', '#/login');
+
+        stubFailure(cy);
+
+        cy.fixture('credentials').then((params) => {
+            cy.contains('Sign in').should("be.visible");
+
+            cy.get("input[placeholder=Email]").type(params.email);
+            cy.get("input[placeholder=Password]").type(params.password + '{ENTER}');
+
+            cy.location('hash').should('eq', '#/login');
+            cy.contains('Your email or password is invalid');
+        });
+
+    });
+
+    it("Redirects to / on auth success", () => {
+        cy.visit("/login");
+        cy.location('hash').should('eq', '#/login');
+
+        stubSuccess(cy);
+
+        cy.fixture('credentials').then((params) => {
+            cy.contains('Sign in').should("be.visible");
+
+            cy.get("input[placeholder=Email]").type(params.email);
+            cy.get("input[placeholder=Password]").type(params.password + '{ENTER}');
+
+            cy.location('hash').should('eq', '#/');
+            cy.contains('John Doe');
+        });
+
+    })
 });
